d5d141fcdd2073de0bdeb30e5425afb11823005c
junius.ang <junius.ang@gmail.com>
2017-01-02 01:03:17 +0700

    Add second command to install apk from build folder

--- rn-cmd.sh
+++ rn-cmd.sh
@@ -2,22 +2,33 @@
 
 VAR_SRC="$HOME/.rncmdrc"
 ADB=$(which adb)
+ADB_CMD="$ADB"
 EMU=$(which emulator)
 EMU_ARGS="-netdelay none -netspeed full -avd"
 #sample
-ANDROID_DIR="/home/junius/project/"
+ANDROID_DIR="/home/junius/project/awesomeproject/android"
+ANDROID_BUILD_VARIANT="generalDebug generalRelease"
 touch "$VAR_SRC"
 source "$VAR_SRC"
 SELECTED_FILE=""
 SELECTED_INPUT=""
+SELECTED_ENVIRONMENT="e"
+IS_CLOSE_AFTER_COMPLETED=0
 
 function cls(){
 	printf "\033c"
 }
 
+function updateADBcommand(){
+	ADB_CMD="$ADB -$SELECTED_ENVIRONMENT"
+}
+
 function runInNewWindow(){
-	echo "cmd : $1"
-	gnome-terminal -e "bash -c \"$1 ; exec bash\""
+	cmd="$1"
+	if [ $IS_CLOSE_AFTER_COMPLETED = 1 ]; then
+		cmd+="; exit 0"
+	fi
+	gnome-terminal -e "bash -c \"echo $cmd & $cmd; read line; exec bash\""
 }
 
 function runInNewWindowSmall(){
@@ -39,6 +50,7 @@ function initScript(){
 
 function menu(){
 	echo "1. START EMULATOR"
+	echo "2. INSTALL APK"
 }
 #$1 = Question string
 function scanDir(){
@@ -52,7 +64,7 @@ function scanDir(){
       exit
 
     elif (( REPLY > 0 && REPLY <= ${#options[@]} )) ; then
-      echo  "You picked $opt which is file $REPLY"
+      #echo  "You picked $opt which is file $REPLY"
 			SELECTED_FILE="$opt"
       break
 
@@ -106,7 +118,16 @@ while $VALID; do
 		if [ ! $SELECTED_INPUT = "" ]; then
 			runInNewWindowSmall "$EMU $EMU_ARGS $SELECTED_INPUT"
 		fi
+
+	elif [ "$chc" = "2" ]; then
+		APK_DIR="$ANDROID_DIR/app/build/outputs/apk"
+		scanDir "$APK_DIR"
+		if [ ! $SELECTED_FILE = "" ]; then
+			updateADBcommand
+			runInNewWindow "$ADB_CMD install -r $SELECTED_FILE"
+		fi
 	fi
+
 done
 echo "$ADB $EMU"
 #scanDir "/home/junius"
